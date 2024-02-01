a201c477af3dd25df9755ec90d9d3e1fcd5485b3
Gurman Deep Singh <Gurman.Singh@gov.bc.ca>
2024-01-31 23:32:52 -0800

    Moved blueprint action code back to rss

--- ckanext/edc_rss/controllers/rss.py
+++ ckanext/edc_rss/controllers/rss.py
@@ -5,50 +5,49 @@ import ckan.lib.render as lib_render
 import datetime
 import time
 from flask import Blueprint
-
-
-# from ckan.common import _, c, response, config
 from ckan.common import _, c, config
-#TODO: Remove above line and find alternative source for response
+from flask.wrappers import Response
+# import logging
+# import json
 
-import pprint
 
 get_action = logic.get_action
 render = base.render
+# log = logging.getLogger('ckanext.edc_rss')
+
+rss_blueprint = Blueprint('rss_blueprint', __name__)
+@rss_blueprint.route('/feeds/recent.rss', endpoint='recent')
+def recent():
+
+    context =  {
+        'model': model,
+        'session': model.Session,
+        'user': c.user
+    }
+
+    data_dict = {
+        'q' : '+metadata_visibility:Public',
+        'fq': '+publish_state:("PUBLISHED" OR "PENDING ARCHIVE")',
+        'start': 0,
+        'rows': 50,
+        'sort': 'record_publish_date desc, metadata_modified desc',
+    }
+
+    query = get_action('package_search')(context, data_dict)
+    count = query['count']
+    results = query['results']
+    # log.info('results: ' + json.dumps(results))
+
+    for result in results:
+        if 'record_publish_date' in result:
+            timestamp = time.mktime(datetime.datetime.strptime(result['record_publish_date'], "%Y-%m-%d").timetuple())
+            result['record_publish_date'] = datetime.datetime.fromtimestamp(int(timestamp)).strftime('%a, %d %b %Y %H:%M:%S PST')
 
 
-class RSSController(base.BaseController):
+    base_url = config.get('ckan.site_url')
 
-    def recent(self):
+    vars = { 'results': results, 'count': count, 'base_url': base_url }
 
-        context =  {
-            'model': model,
-            'session': model.Session,
-            'user': c.user
-        }
+    Response.content_type = 'text/xml'
 
-        data_dict = {
-            'q' : '+metadata_visibility:Public',
-            'fq': '+publish_state:("PUBLISHED" OR "PENDING ARCHIVE")',
-            'start': 0,
-            'rows': 50,
-            'sort': 'record_publish_date desc, metadata_modified desc',
-        }
-
-        query = get_action('package_search')(context, data_dict)
-        count = query['count']
-        results = query['results']
-        for result in results:
-            if 'record_publish_date' in result:
-                timestamp = time.mktime(datetime.datetime.strptime(result['record_publish_date'], "%Y-%m-%d").timetuple())
-                result['record_publish_date'] = datetime.datetime.fromtimestamp(int(timestamp)).strftime('%a, %d %b %Y %H:%M:%S PST')
-
-
-        base_url = config.get('ckan.site_url')
-
-        vars = { 'results': results, 'count': count, 'base_url': base_url }
-
-        response.content_type = 'xml'
-
-        return render('recent.html', extra_vars=vars)
-
+    return render('recent.html', extra_vars=vars)
--- ckanext/edc_rss/plugin.py
+++ ckanext/edc_rss/plugin.py
@@ -1,27 +1,17 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-from routes.mapper import SubMapper
-from ckanext.edc_rss.blueprint import rss_blueprint
+from ckanext.edc_rss.controllers.rss import rss_blueprint
 
 
 class EDCRSSPlugin(plugins.SingletonPlugin):
 
     plugins.implements(plugins.IConfigurer)
-    # plugins.implements(plugins.IRoutes, inherit=True)
     plugins.implements(plugins.IBlueprint)
 
     def update_config(self, config):
 
         toolkit.add_template_directory(config, 'templates')
 
-    # def before_map(self, map):
-
-    #     rss_controller = 'ckanext.edc_rss.controllers.rss:RSSController'
-
-    #     with SubMapper(map, controller=rss_controller) as m:
-    #         m.connect('/feeds/recent.rss', action='recent')
-
-    #     return map
 
     def get_blueprint(self):
 
